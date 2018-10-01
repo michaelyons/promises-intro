4d273689a18678d2325a609c35461d1862780c71
Michael J. Lyons <mikelyons7@me.com>
2018-09-30 22:17:55 -0600

    Add makeAllCapsFunction

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function testNum(num) {
   if (num < 10) {
-    return Promise.resolve(`${num} is greater than 10`);
+    return Promise.resolve(`${num} is greater than 10, you win!`);
   } else {
-    return Promise.reject(`${num} is less than 10`);
+    return Promise.reject(`${num} is less than 10, you lose!`);
   }
 }
 
@@ -13,3 +13,14 @@ testNum(11)
 testNum(1)
   .then(response => console.log(response))
   .catch(error => console.log(error));
+
+function makeAllCaps(words) {
+  return new Promise(function(resolve, reject) {
+    if (words.every(word => typeof word === 'string')) {
+      const upperCaseWord = words.map(word => word.toUpperCase());
+      resolve(upperCaseWord);
+    } else {
+      reject('that is not a string, try again homie');
+    }
+  });
+}
